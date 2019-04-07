6ab803a45cb2b509749106eeda7bc7731d4f0974
xws-experimental <49362773+xws-experimental@users.noreply.github.com>
2019-04-07 17:36:48 +0200

    Added (unfinished) Servomotor S-Foils

--- src/upgcards.js
+++ src/upgcards.js
@@ -7373,7 +7373,52 @@ var UPGRADES=window.UPGRADES= [
 		desactivate: function() {
 			return false;
 		}
-	}
+	},
+	{
+	     name:"Servomotor S-Foils",
+	     type:Unit.MOD,
+	     points:0,
+	     ship:"X-Wing",
+	     canbeswitched:false,
+	     done:false,
+	     canswitch: function() {
+		 return this.canbeswitched;
+	     },
+	     switch: function() {
+		 var u=this.unit;
+		 this.canbeswitched=false;
+		 if (this.faceup) {
+		     if (typeof u.getagility.unwrap!="undefined") u.getagility.unwrap(this);
+		     var save=[];
+		     sh.installed=true;
+		     sh.wrap_after("getdial",this,function(gd) {
+		     	if (save.length===0) 
+				for (var i=0; i<gd.length; i++) {
+					var move=gd[i].move;
+					var d=gd[i].difficulty;
+					if (move.match(/BL\d|BR\d/)) d="GREEN";
+					save[i]={move:move,difficulty:d};
+				}
+				return save;
+		     });
+		     this.variant="Landing";
+		 } else {
+		     if (typeof u.getmaneuverlist.unwrap!="undefined") u.getmaneuverlist.unwrap(this);
+		     u.wrap_after("getattack",this,function(a) {
+			 return a+1;
+		     });
+		     this.variant="Attack";
+		 }
+		 this.faceup=!this.faceup;    
+	     },
+	     init: function(sh) {
+		 var self=this;
+		 this.switch();
+		 this.unit.wrap_before("beginactivationphase",this,function() {
+		     self.canbeswitched=true;
+		 });
+	     }
+    	},
 ];
 var UPGRADESNAMEINDEX=window.UPGRADESNAMEINDEX=UPGRADES.map(function(upgcrd){
     if(upgcrd.ambiguous===true&&typeof upgcrd.edition!=="undefined") return upgcrd.name.replace(/\'/g,"")+" ("+upgcrd.edition+")";
